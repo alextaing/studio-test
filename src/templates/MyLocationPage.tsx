d60824834e7d702abc166b042d39d9b88b6d6e71
Yext Studio <studio-placeholder@yext.com>
2023-08-09 14:20:07 -0400

    Yext Studio Commit

---
+++ src/templates/MyLocationPage.tsx
@@ -0,0 +1,34 @@
+import { GetPath, TemplateConfig, TemplateProps } from "@yext/pages";
+import Banner from "../components/Banner";
+import BigButton from "../components/BigButton";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import Paragraph from "../components/Paragraph";
+
+export const config: TemplateConfig = {
+  stream: {
+    $id: "studio-stream-id-MyLocationPage",
+    localization: { locales: ["en"], primary: false },
+    filter: { entityTypes: ["location"] },
+    fields: ["name", "address", "isoRegionCode"],
+  },
+};
+export const getPath: GetPath<TemplateProps> = () => {
+  return `location123`;
+};
+
+export default function MyLocationPage({ document }: TemplateProps) {
+  return (
+    <>
+      <Banner />
+      <Card title={`${document.name}`} url={`${document.address.city}`} />
+      <BigButton title={`Button Title ${document.name}`} href="#" />
+      <Paragraph
+        value={`Paragraph ${document.address.city} ${document.isoRegionCode} ${document.locale} ${document.key}`}
+        textSize="base"
+        fontWeight="normal"
+      />
+      <Footer />
+    </>
+  );
+}
